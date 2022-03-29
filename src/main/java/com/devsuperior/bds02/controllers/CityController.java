b1593f14efaace4e2a2232221ee93409aefe04ad
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-29 10:55:20 -0300

    Insert new city

--- src/main/java/com/devsuperior/bds02/controllers/CityController.java
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.bds02.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
@@ -27,4 +29,15 @@ public class CityController {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@PostMapping
+	public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto){
+		
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
 }
+
--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -25,4 +25,15 @@ public class CityService {
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
 	}
 
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		
+		City entity = new City();
+		entity.setId(dto.getId());
+		entity.setName(dto.getName());
+		
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+
 }
